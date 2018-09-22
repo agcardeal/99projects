6a37ecd2744a5439c95a8a0f8cbfb93a7c33b5f8
agcardeal <43125193+agcardeal@users.noreply.github.com>
2018-09-22 13:19:00 -0300

    update
    update

--- globalinvestidores.js
+++ globalinvestidores.js
@@ -1,1 +1,33 @@
-console.log('teste')
+window.onload = function(){
+
+
+
+/* MENU MOBILE */
+var el = document.querySelector('.c-hamburger');
+var h =  document.querySelector('#header');
+var nav =  document.querySelector('#menu');
+var ham = document.querySelector('.c-hamburger--htx') ;
+
+el.onclick = function() { 
+	
+  el.classList.toggle('is-active');
+  if (el.classList.contains('is-active')) {		
+		showMenu(nav);
+		
+	}else{		
+		hideMenu(nav);		
+	}
+}
+
+function showMenu(el){
+	jQuery(el).addClass('open');
+	jQuery(h).addClass('open');
+}
+
+function hideMenu(el){
+	jQuery(el).removeClass('open');
+	jQuery(h).removeClass('open');
+	jQuery(ham).removeClass('is-active');
+}
+
+}
