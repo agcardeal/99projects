04c9f824b9b0fefdd6b168ed586e175fd6ab682f
agcardeal <43125193+agcardeal@users.noreply.github.com>
2018-09-29 15:29:04 -0300

    start
    start

---
+++ globalinvestidores4.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+
+
+
+console.log('teste');
+
+/* MENU MOBILE */
+var el = document.querySelector('.c-hamburger');
+var h =  document.querySelector('#header');
+var nav =  document.querySelector('#menu');
+var ham = document.querySelector('.c-hamburger--htx') ;
+
+el.onclick = function() { 
+  
+  el.classList.toggle('is-active');
+  if (el.classList.contains('is-active')) {   
+    showMenu(nav);
+    
+  }else{    
+    hideMenu(nav);    
+  }
+};
+
+function showMenu(el){
+  jQuery(el).addClass('open');
+  jQuery(h).addClass('open');
+};
+
+function hideMenu(el){
+  jQuery(el).removeClass('open');
+  jQuery(h).removeClass('open');
+  jQuery(ham).removeClass('is-active');
+};
+  
+// modal
+var exibirModal = function(){
+  jQuery('#modal-inscreva-se').show(1000);
+};
+
+var fecharModal = function(){
+  jQuery('#modal-inscreva-se').fadeOut(1000);
+};
+  
+  setTimeout(function(){ exibirModal(); }, 40000);
+  
+  jQuery('#modal-inscreva-se .fechar').on('click', fecharModal);
+    
+
+}
+
+
+
+
